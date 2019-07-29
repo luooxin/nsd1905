4304f0809f796f722477347d9d7b7b63124b5f3c
Luo Xin <luoxin@example.com>
2019-07-29 10:10:32 +0800

    gengxin

--- 1-nginx服务器.sh
+++ 1-nginx服务器.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #nginx服务器
+checkyum(){
 yum clean all &> /dev/null
 yumr=`yum repolist 2> /dev/null | awk '/^repo/{print $2}' | sed 's/,//'`
 if [ $yumr -eq 0 ];then
       echo -e "\033[31myum仓库不可用\033[0m"
         exit
 fi
+}
+
+checklnmptar(){
 tar -xf lnmp_soft.tar.gz  &> /dev/null
 if [ $? -ne 0 ];then
      echo -e "\033[31m未找到lnmp_soft.tar.gz或不存在\033[0m"
      exit
 fi
+}
+
+Compileandinstall(){
 cd lnmp_soft
 tar -xf nginx-1.12.2.tar.gz
 cd nginx-1.12.2
@@ -28,10 +35,19 @@ echo -e "\033[31m已部署php服务\033[0m"
 make   &> /dev/null
 make install  &> /dev/null
 echo -e "\033[31mnginx源码编译安装成功\033[0m"
+}
+
+configure(){
 conf="/usr/local/nginx/conf"
 sed -i '65,71s/#//' $conf/nginx.conf
 sed -i '/SCRIPT_FILENAME/d'  $conf/nginx.conf
 sed -i 's/fastcgi_params/fastcgi.conf/'  $conf/nginx.conf
+}
+
+checkyum
+checklnmptar
+Compileandinstall
+configure
 /usr/local/nginx/sbin/nginx  &> /dev/null
 ss -nutpl | grep :80   &> /dev/null
 if [ $? -ne 0 ];then
