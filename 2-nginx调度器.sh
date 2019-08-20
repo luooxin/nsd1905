ad0bd38862a3d6dc9d8e31b7747aa1a10a40206f
Luo Xin <luoxin@example.com>
2019-08-20 20:33:54 +0800

    11

--- 2-nginx调度器.sh
+++ 2-nginx调度器.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-#快速部署nginx调度器
-#未附加权重功能
+checkyum(){
 yum clean all &> /dev/null
 yumr=`yum repolist 2> /dev/null | awk '/^repo/{print $2}' | sed 's/,//'`
 if [ $yumr -eq 0 ];then
       echo -e "\033[31myum仓库不可用\033[0m"
-      exit
+        exit
 fi
-tar -xf lnmp_soft.tar.gz  &> /dev/null
-if [ $? -ne 0 ];then
-     echo -e "\033[31m未找到lnmp_soft.tar.gz或不存在\033[0m"
-     exit
+}
+
+checknginx(){
+if [ ! -f nginx-1.12.2.tar.gz ];then
+echo -e "\033[31m未找到nginx源码包,脚本结束运行\033[0m"
+exit
 fi
-cd lnmp_soft
+}
+
+nginxaz(){
 tar -xf nginx-1.12.2.tar.gz
 cd nginx-1.12.2
 yum -y install gcc pcre-devel openssl-devel  &> /dev/null
 echo -e "\033[31m已安装源码编译依赖包\033[0m"
-#yum -y install mariadb mariadb-server mariadb-devel &> /dev/null
-#systemctl restart mariadb
-#systemctl enable mariadb  &> /dev/null
-#echo -e "\033[31m已部署数据库服务\033[0m"
-#yum -y install php php-fpm php-mysql &> /dev/null
-#systemctl restart php-fpm
-#systemctl enable php-fpm &> /dev/null
-#echo -e "\033[31m已部署php服务\033[0m"
 ./configure --with-http_ssl_module --with-stream --with-http_stub_status_module  &> /dev/null
 make   &> /dev/null
 make install  &> /dev/null
 echo -e "\033[31mnginx源码编译安装成功\033[0m"
+}
+
+gaipeizhi(){
 conf="/usr/local/nginx/conf"
 for i in {1..100}
 do
@@ -50,4 +48,13 @@ do
 sleep 1
 echo -ne "\b\b$sj"
 done
+}
 
+echo "执行该脚本需要准备可用yum源及nginx-1.12.2.tar.gz"
+read -p "是否需要继续执行该脚本[y/n]" n
+if [ $n == "y" ];then
+checkyum
+checknginx
+nginxaz
+gaipeizhi
+fi
